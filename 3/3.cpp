0b19b4042c79c0ce27192b483d557a9290b0a914
Pavel Karpiy <pasha.karpiy98@gmail.com>
2019-12-30 10:10:43 +0300

    3: fix output

--- 3/3.cpp
+++ 3/3.cpp
@@ -8,35 +8,25 @@
 using namespace std;
 
 const int N = 8;
-const int num_of_threads = 8;
+int num_of_threads = 8;
 
-void rand_init(float** Matr, float* vect, float* result_vect) {
-    srand(time(NULL));
+
+void print_matrix(float** Matrix) {
     for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            Matr[i][j] = rand() % 10;
-        }
-        vect[i] = rand() % 10;
-        result_vect[i] = 0;
-    }
-}
-
-void print_matrix(float** Matr, int dim1, int dim2) {
-    for (int i = 0; i < dim1; ++i) {
-        for (int j = 0; j < dim2; ++j)
-            cout << setw(1) << Matr[i][j] << " ";
+        for (int j = 0; j < N; ++j)
+            cout << setw(1) << Matrix[i][j] << " ";
         cout << endl;
     }
 }
 
-void print_vector(float* vec, int dim) {
-    for (int i = 0; i < dim; ++i) {
-        cout << setw(2) << vec[i] << " ";
+void print_vector(float* vector) {
+    for (int i = 0; i < N; ++i) {
+        cout << setw(2) << vector[i] << " ";
     }
     cout << endl;
 }
 
-double serial_calculate(float** Matr, float* vect, float* result_vect) {
+double serial_calculate(float** Matr, const float* vect, float* result_vect) {
     int i, j;
     double time = omp_get_wtime();
     for (i = 0; i < N; ++i) {
@@ -46,15 +36,14 @@ double serial_calculate(float** Matr, float* vect, float* result_vect) {
     return omp_get_wtime() - time;
 }
 
-// Data separation by rows
-double parallel_raw_calculate(float** Matr, float* vect, float* result_vect) {
+double parallel_raw_calculate(float** Matr, const float* vect, float* result_vect) {
     int i, j;
     float sum = 0;
     omp_set_num_threads(num_of_threads);
     double time = omp_get_wtime();
     for (i = 0; i < N; ++i) {
         sum = 0;
-    #pragma omp paralell for redunction(+:sum)
+    #pragma omp paralell for redunction(+ : sum)
         for (j = 0; j < N; ++j)
             sum += Matr[i][j] * vect[j];
         result_vect[i] = sum;
@@ -62,7 +51,7 @@ double parallel_raw_calculate(float** Matr, float* vect, float* result_vect) {
     return omp_get_wtime() - time;
 }
 
-double parallel_column_calculate(float** Matr, float* vect, float* result_vect) {
+double parallel_column_calculate(float** Matr, const float* vect, float* result_vect) {
     omp_set_num_threads(num_of_threads);
     auto** partial_sums = new float*[N];
     for(int i = 0 ; i < N ; ++i)
@@ -77,7 +66,7 @@ double parallel_column_calculate(float** Matr, float* vect, float* result_vect) 
             IterResult = 0;
             for (int j = 0; j < block_size; ++j)
                 IterResult += Matr[i][j +
-                                   thread_id*block_size] *
+                                      thread_id*block_size] *
                               vect[j + thread_id*block_size];
             partial_sums[thread_id][i] = IterResult;
         }
@@ -88,7 +77,7 @@ double parallel_column_calculate(float** Matr, float* vect, float* result_vect) 
     return omp_get_wtime() - time;
 }
 
-double parallel_block_calculation(float** Matr, float* vect, float* result_vect) {
+double parallel_block_calculation(float** Matr, const float* vect, float* result_vect) {
     omp_set_num_threads(num_of_threads);
     omp_set_nested(true);
     double time = omp_get_wtime();
@@ -103,33 +92,48 @@ double parallel_block_calculation(float** Matr, float* vect, float* result_vect)
     return omp_get_wtime() - time;
 }
 
+void init_with_random_values(float** Matr, float* vect) {
+    srand(time(NULL));
+    for (int i = 0; i < N; ++i) {
+        for (int j = 0; j < N; ++j) {
+            Matr[i][j] = rand() % 10;
+        }
+        vect[i] = rand() % 10;
+    }
+}
+
 
 int main() {
-    auto** Matrix = new float*[N];
+    auto** A = new float*[N];
     for(int i = 0 ; i < N ; ++i)
-        Matrix[i] = new float[N];
-    auto* vect = new float[N];
+        A[i] = new float[N];
+    auto* b = new float[N];
     auto* serial_result = new float[N];
     auto* row_result = new float[N];
     auto* column_result = new float[N];
     auto* block_result = new float[N];
 
-    rand_init(Matrix, vect, serial_result);
-    double serial_time = serial_calculate(Matrix, vect, serial_result);
-    double parallel_raw = parallel_raw_calculate(Matrix, vect, row_result);
-    double parallel_column = parallel_column_calculate(Matrix, vect, column_result);
-    double parallel_block = parallel_block_calculation(Matrix, vect, block_result);
+    init_with_random_values(A, b);
 
-    cout  << "Matrix:" << endl;
-    print_matrix(Matrix, N, N);
-    cout << "Vector:" << endl;
-    print_vector(vect, N);
-    cout << "Serial result: (time: " << serial_time << ")" << endl;
-    print_vector(serial_result, N);
-    cout << "Raw result: (time: " << parallel_raw << ")" << endl;
-    print_vector(row_result, N);
-    cout << "Colomn result: (time: " << parallel_column << ")" << endl;
-    print_vector(column_result, N);
-    cout << "Block result: (time: " << parallel_block << ")" << endl;
-    print_vector(block_result, N);
+    for (int i = 2; i <= 8; ++i) {
+        num_of_threads = i;
+        double serial_time = serial_calculate(A, b, serial_result);
+        double parallel_raw = parallel_raw_calculate(A, b, row_result);
+        double parallel_column = parallel_column_calculate(A, b, column_result);
+        double parallel_block = parallel_block_calculation(A, b, block_result);
+        cout << "Results for " << i << " threads:" << endl;
+//        cout  << "A:" << endl;
+//        print_matrix(A);
+//        cout << "Vector:" << endl;
+//        print_vector(b);
+        cout << "Serial result: " << serial_time << ")" << endl;
+//        print_vector(serial_result);
+        cout << "Raw result: " << parallel_raw << ")" << endl;
+//        print_vector(row_result);
+        cout << "Colomn result: " << parallel_column << ")" << endl;
+//        print_vector(column_result);
+        cout << "Block result: " << parallel_block << ")" << endl;
+//        print_vector(block_result);
+        cout << "===================================" << endl;
+    }
 }
